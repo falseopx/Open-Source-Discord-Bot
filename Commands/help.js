7abd2e0433fa6b014c4a0e7488170e6ca0c7f1a1
Branta <56791945+AfkBranta@users.noreply.github.com>
2020-09-05 14:59:35 -0400

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -21,7 +21,8 @@ module.exports = {
             { name: 'Uptime', value: 'Replies With The Total Uptime Of The Bot, usage: ~uptime', inline: true },
             { name: '8Ball', value: 'Replies With Your Future, usage: ~8ball [question]', inline: true },
             { name: 'Rate', value: 'Rates A Person Of Your Choosing, usage: ~rate [person]', inline: true },
-            { name: 'Kill', value: 'Get The Bot To Kill Someone, usage: ~kill [person', inline: true }
+            { name: 'Kill', value: 'Get The Bot To Kill Someone, usage: ~kill [person', inline: true },
+            { name: 'Coin', value: 'Flip A Coin, usage: ~coin', inline: true }
         )
         .setAuthor('Cub')
         .setColor('BLUE')
