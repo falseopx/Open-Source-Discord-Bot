c753780f1caf6b8dab42268d836c6e92ecdb3170
Branta <56791945+AfkBranta@users.noreply.github.com>
2020-09-05 16:15:21 -0400

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -7,8 +7,8 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setTitle('`Commands:`')
         .addFields(
-            { name: 'Help Mod', value: 'Miscellaneous Commands, usage: ~help mod', inline: true }
-            { name: 'Help Fun', value: 'Miscellaneous Commands, usage: ~help fun', inline: true }
+            { name: 'Help Mod', value: 'Miscellaneous Commands, usage: ~help mod', inline: true },
+            { name: 'Help Fun', value: 'Miscellaneous Commands, usage: ~help fun', inline: true },
             { name: 'Help Misc', value: 'Miscellaneous Commands, usage: ~help misc', inline: true }
         )
         .setAuthor('Cub')
