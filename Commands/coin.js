d679aa119f5f68d481c4cc7a0c9be714bee2aa1f
Branta <56791945+AfkBranta@users.noreply.github.com>
2020-09-05 14:59:09 -0400

    added a coin command

---
+++ Commands/coin.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "coin",
+
+    execute(message, args){
+        let number = Math.floor(Math.random() * 2);
+        if (number == 1) {
+            message.channel.send('Heads')
+        }
+        if (number == 0) {
+            message.channel.send('Tails')
+        }
+    }
+}
