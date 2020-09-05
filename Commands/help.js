ae7683c94c081a268becb5953feb410bc70945d5
Branta <56791945+AfkBranta@users.noreply.github.com>
2020-09-05 14:50:11 -0400

    added kill command

--- Commands/help.js
+++ Commands/help.js
@@ -20,7 +20,8 @@ module.exports = {
             { name: 'Memes', value: 'Sends A Dank Meme, usage: ~meme', inline: true },
             { name: 'Uptime', value: 'Replies With The Total Uptime Of The Bot, usage: ~uptime', inline: true },
             { name: '8Ball', value: 'Replies With Your Future, usage: ~8ball [question]', inline: true },
-            { name: 'Rate', value: 'Rates A Person Of Your Choosing, usage: ~rate [person]', inline: true }
+            { name: 'Rate', value: 'Rates A Person Of Your Choosing, usage: ~rate [person]', inline: true },
+            { name: 'Kill', value: 'Get The Bot To Kill Someone, usage: ~kill [person', inline: true }
         )
         .setAuthor('Cub')
         .setColor('BLUE')
